d2d92e94d06f823bd059794b30842f081b541c6f
froatsnook <froatsnook@gmail.com>
2015-01-25 16:42:14 +0100

    Update package name and git.

--- package.js
+++ package.js
@@ -1,8 +1,8 @@
 Package.describe({
 	summary: "A Simplified HTTP client",
 	version: "2.40.0",
-	git: "https://github.com/czbaker/meteor-request.git",
-	name: "czbaker:request"
+	git: "https://github.com/froatsnook/meteor-request.git",
+	name: "froatsnook:request"
 });
 
 Npm.depends({
