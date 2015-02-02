aa10f54c6ca299ec11d7806846eac746b541211a
froatsnook <froatsnook@gmail.com>
2015-02-02 11:33:57 +0100

    Bump version to 2.52.0.

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
 	summary: "A Simplified HTTP client (forked for platform independence)",
-	version: "2.51.1",
+	version: "2.52.0",
 	git: "https://github.com/froatsnook/meteor-request.git",
 	name: "froatsnook:request"
 });
