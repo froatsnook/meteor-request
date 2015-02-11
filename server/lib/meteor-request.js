43caa9d976899856cdaa37e303c18f73d5d8256c
froatsnook <froatsnook@gmail.com>
2015-02-11 17:00:35 +0100

    Convert tabs to spaces.

--- server/lib/meteor-request.js
+++ server/lib/meteor-request.js
@@ -3,80 +3,80 @@ request = Npm.require("request");
 
 // Wrap request with something that can be `Meteor.wrapAsync`ed.
 var requestAsync = function(uri, options, callback) {
-	if (typeof uri === "function") {
-		callback = uri;
-		uri = undefined;
-		options = undefined;
-	}
+    if (typeof uri === "function") {
+        callback = uri;
+        uri = undefined;
+        options = undefined;
+    }
 
-	if (typeof options === "function") {
-		callback = options;
-		options = undefined;
-	}
+    if (typeof options === "function") {
+        callback = options;
+        options = undefined;
+    }
 
-	request(uri, options, function(error, response, body) {
-		if (error) {
-			callback(error);
-		} else {
-			callback(null, {
-				response: response,
-				body: body
-			});
-		}
-	});
+    request(uri, options, function(error, response, body) {
+        if (error) {
+            callback(error);
+        } else {
+            callback(null, {
+                response: response,
+                body: body
+            });
+        }
+    });
 };
 
 // Make a sync function out of it.
 var requestSync;
 if (typeof Meteor.wrapAsync === "function") {
-	requestSync = Meteor.wrapAsync(requestAsync);
+    requestSync = Meteor.wrapAsync(requestAsync);
 } else {
-	requestSync = Meteor._wrapAsync(requestAsync);
+    requestSync = Meteor._wrapAsync(requestAsync);
 }
 
 // Use this to extend the prototype
 _.extend(request, {
 
-	putSync: function(uri, options) {
-		if (!options) {
-			options = { };
-		}
-		options.method = "PUT";
-		return requestSync(uri, options);
-	},
+    putSync: function(uri, options) {
+        if (!options) {
+            options = { };
+        }
+        options.method = "PUT";
+        return requestSync(uri, options);
+    },
 
-	patchSync: function(uri, options) {
-		if (!options) {
-			options = { };
-		}
-		options.method = "PATCH";
-		return requestSync(uri, options);
-	},
+    patchSync: function(uri, options) {
+        if (!options) {
+            options = { };
+        }
+        options.method = "PATCH";
+        return requestSync(uri, options);
+    },
 
-	postSync: function(uri, options) {
-		if (!options) {
-			options = { };
-		}
-		options.method = "POST";
-		return requestSync(uri, options);
-	},
+    postSync: function(uri, options) {
+        if (!options) {
+            options = { };
+        }
+        options.method = "POST";
+        return requestSync(uri, options);
+    },
 
-	headSync: function(uri, options) {
-		if (!options) {
-			options = { };
-		}
-		options.method = "HEAD";
-		return requestSync(uri, options);
-	},
+    headSync: function(uri, options) {
+        if (!options) {
+            options = { };
+        }
+        options.method = "HEAD";
+        return requestSync(uri, options);
+    },
 
-	delSync: function(uri, options) {
-		if (!options) {
-			options = { };
-		}
-		options.method = "DELETE";
-		return requestSync(uri, options);
-	},
+    delSync: function(uri, options) {
+        if (!options) {
+            options = { };
+        }
+        options.method = "DELETE";
+        return requestSync(uri, options);
+    },
 
-	getSync: requestSync
+    getSync: requestSync
 
 });
