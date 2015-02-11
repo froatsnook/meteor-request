8eebc82063102b88ab8847738e8c1b4465c6743b
froatsnook <froatsnook@gmail.com>
2015-02-11 17:45:08 +0100

    Add request.defaults support.

--- server/lib/meteor-request.js
+++ server/lib/meteor-request.js
@@ -2,7 +2,18 @@
 request = Npm.require("request");
 
 // Wrap request with something that can be `Meteor.wrapAsync`ed.
-var requestAsync = function(uri, options, callback) {
+//
+// @param r
+//   request object (either request itself or wrapper returned by
+//   request.defaults).  Since this is a function, either uri or options must
+//   be given or else Meteor.wrapAsync will perform strangely.
+// @param uri
+//   Fully qualified uri or a parsed url object from url.parse()
+// @param [options] {Object}
+//   Request options.  See https://github.com/request/request#requestoptions-callback
+// @param callback {Function}
+//   Meteor.wrapAsync's callback function.
+var requestAsync = function(r, uri, options, callback) {
     if (typeof uri === "function") {
         callback = uri;
         uri = undefined;
@@ -14,7 +25,7 @@ var requestAsync = function(uri, options, callback) {
         options = undefined;
     }
 
-    request(uri, options, function(error, response, body) {
+    r(uri, options, function(error, response, body) {
         if (error) {
             callback(error);
         } else {
@@ -26,7 +37,7 @@ var requestAsync = function(uri, options, callback) {
     });
 };
 
-// Make a sync function out of it.
+// Make a sync function out of requestAsync.
 var requestSync;
 if (typeof Meteor.wrapAsync === "function") {
     requestSync = Meteor.wrapAsync(requestAsync);
@@ -34,49 +45,65 @@ if (typeof Meteor.wrapAsync === "function") {
     requestSync = Meteor._wrapAsync(requestAsync);
 }
 
-// Use this to extend the prototype
-_.extend(request, {
-
-    putSync: function(uri, options) {
-        if (!options) {
-            options = { };
+// Add sync methods to a request-like object (`request` itself or anything
+// returned by `request.defaults`).
+var copySyncMethods = function(r) {
+    _.extend(r, {
+        putSync: function(uri, options) {
+            if (!options) {
+                options = { };
+            }
+            options.method = "PUT";
+            return requestSync(r, uri, options);
+        },
+        patchSync: function(uri, options) {
+            if (!options) {
+                options = { };
+            }
+            options.method = "PATCH";
+            return requestSync(r, uri, options);
+        },
+        postSync: function(uri, options) {
+            if (!options) {
+                options = { };
+            }
+            options.method = "POST";
+            return requestSync(r, uri, options);
+        },
+        headSync: function(uri, options) {
+            if (!options) {
+                options = { };
+            }
+            options.method = "HEAD";
+            return requestSync(r, uri, options);
+        },
+        delSync: function(uri, options) {
+            if (!options) {
+                options = { };
+            }
+            options.method = "DELETE";
+            return requestSync(r, uri, options);
+        },
+        getSync: function(uri, options) {
+            if (!options) {
+                options = { };
+            }
+            return requestSync(r, uri, options);
         }
-        options.method = "PUT";
-        return requestSync(uri, options);
-    },
+    });
+};
 
-    patchSync: function(uri, options) {
-        if (!options) {
-            options = { };
-        }
-        options.method = "PATCH";
-        return requestSync(uri, options);
-    },
+// Add sync methods to global request object.
+copySyncMethods(request);
 
-    postSync: function(uri, options) {
-        if (!options) {
-            options = { };
-        }
-        options.method = "POST";
-        return requestSync(uri, options);
-    },
+// request.defaults returns a wrapper around the normal request API that
+// defaults to whatever options you pass to it.  It doesn't have getSync and
+// friends.  So replace request.defaults with a shim wrapper makes sure that
+// getSync and friends are added to the returned request wrapper.
+var originalDefaults = request.defaults;
+request.defaults = function(options) {
+    var defaultedRequest = originalDefaults.call(request, options);
+    copySyncMethods(defaultedRequest);
+    return defaultedRequest;
+};
 
-    headSync: function(uri, options) {
-        if (!options) {
-            options = { };
-        }
-        options.method = "HEAD";
-        return requestSync(uri, options);
-    },
-
-    delSync: function(uri, options) {
-        if (!options) {
-            options = { };
-        }
-        options.method = "DELETE";
-        return requestSync(uri, options);
-    },
-
-    getSync: requestSync
-
-});
