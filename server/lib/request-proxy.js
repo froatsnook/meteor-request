18836253353ee59329e7f27220db6b1be18f85e6
froatsnook <froatsnook@gmail.com>
2015-06-16 13:32:37 +0200

    Remove dead code.

--- server/lib/request-proxy.js
+++ server/lib/request-proxy.js
@@ -3,20 +3,6 @@ Meteor.methods({
         var result = request.sync(options);
 
         var cleanResponse = JSON.parse(JSON.stringify(result.response));
-
-        //var cleanResponse = _.omit(result.response, function(val, key) {
-        //    // Don't send "private API" response parts
-        //    if (key.indexOf("_") === 0) {
-        //        return true;
-        //    }
-
-        //    // Don't send functions
-        //    if (_.isFunction(val)) {
-        //        return true;
-        //    }
-
-        //});
-
         var cleanBody = result.body;
 
         // Strip away parts that can't be sent to the client, like functions.
