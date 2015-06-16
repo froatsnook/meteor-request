23ce264cc69f7fb4df38c50c26000f7aea67afbd
froatsnook <froatsnook@gmail.com>
2015-06-16 13:35:54 +0200

    Bump version to 2.58.0.

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     summary: "A Simplified HTTP client which supports retrieving binary data",
-    version: "2.57.0",
+    version: "2.58.0",
     git: "https://github.com/froatsnook/meteor-request.git",
     name: "froatsnook:request"
 });
